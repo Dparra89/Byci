46b17805b21104d63e958a7af6207ab39927b066
Dparra89 <dani.k.parra@gmail.com>
2024-06-02 18:00:43 -0300

    ajustes clase 13

---
+++ clase 13/ejemplo01.js
@@ -0,0 +1,25 @@
+/*
+var
+let
+const
+*/
+
+const nombre = 'Daniela'
+const apellido = 'Parra'
+const edad = 35
+
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+console.log(`Mi nombre es ${nombre} y tengo ${edad}`);
+
+document.write(`Mi nombre es ${nombre} y tengo ${edad}`)
+
+console.log(nombre);
+
+
+
+
+
---
+++ clase 13/ejemplo04.js
@@ -0,0 +1,16 @@
+const frutas =['Bananas', 'Manzanas', 'Pera', 'Palta'];
+
+const numeros=[12,26,59,78];
+
+console.log(frutas);
+console.log(numeros);
+
+console.log(frutas.length); //4
+console.log(frutas[0]); //banana
+
+const ultimo = frutas[frutas.length -1] //Palta: 4-1=3 
+
+console.log(ultimo);
+
+
+document.write(frutas);
